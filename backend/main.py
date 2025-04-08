4eeb0ba1cac1c1058207d0911bf7bcf85e894008
Indranil2003 <indranilthakare2003@gmail.com>
2025-04-08 19:19:29 +0530

    fix: resolve Railway deployment crash
    Moved main.py to backend root and updated nixpacks.toml to reflect correct startup path

--- backend/main.py
+++ backend/main.py
@@ -40,4 +40,5 @@ def recommend_assessments(request: QueryRequest):
     return {"recommendations": results}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    # uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
